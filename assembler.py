e518c9cf3d836dd4095b5a16dcd4fcfa238c8b36
Harsh <harsh24241@iiitd.ac.in>
2025-02-17 18:37:28 +0530

    basic structure for assembler

--- assembler.py
+++ assembler.py
@@ -1,12 +1,87 @@
-<<<<<<< HEAD
-#starting of the assembler project code now 
+import re
 
-def tokenise(s):
-    #function to select keywords
-    pass
-=======
-#starting of the assembler project code now 
+class Rtype:
+    encoding={}
+    def __inti__(self,line ):
+        pass
+    def code(self):
+        pass
+class Itype:
+    encoding={}
+    def __inti__(self,line ):
+        pass
+    def code(self):
+        pass
+class Ltype:
+    encoding={}
+    def __inti__(self,line ):
+        pass
+    def code(self):
+        pass
+class Btype:
+    encoding={}
+    def __inti__(self,line ):
+        pass
+    def code(self):
+        pass
+class Jtype:
+    encoding={}
+    def __inti__(self,line ):
+        pass
+    def code(self):
+        pass
 
-def tokenise(s):
-    pass
->>>>>>> 0d175e0041e927608b5bda06b7ee24282760b804
+
+
+#starting of the assembler project code now 
+def identity(i):
+    i=i.rstrip()
+    if i=="":
+        return " "
+    if(i[0]==" "):return "error"
+
+    #using re python library to match strings
+    rsyntax = r"^(add|sub|and|or|slt|srl) (\w+),(\w+),(\w+)$"
+    isyntax = r"^(addi|jalr) (\w+),(\w+),(-?\d+)$"
+    ssyntax = r"^(sw|lw) (\w+),(\d+)\((\w+)\)$"
+    bsyntax = r"^(beq|bne|blt) (\w+),(\w+),(-?\d+)$"
+    #usyntax = r"^(lui|auipc) (\w+),(\d+)$"
+    jsyntax = r"^(jal) (\w+),(-?\d+)$"
+
+    # passing line by line through instruction array to checking it's type
+    if re.match(rsyntax, i):
+        return "R"
+    elif re.match(isyntax, i):
+        return "I"
+    elif re.match(ssyntax, i):
+        return "S"
+    elif re.match(bsyntax, i):
+        return "B"
+    # elif re.match(usyntax, i):
+    #     return "U"
+    elif re.match(jsyntax, i):
+        return "J"
+    else:
+        return "error"
+
+def debug(l):
+    error=[]
+    for i in range(len(l)):
+        if identity(l[i])=="error":
+            error.append(f"line:{i+1} syntax error! invalid syntax for  RISC ISA")
+    return error
+
+
+
+def read_assembly(filename):
+    with open(filename,"r") as f:
+        l=[x.rstrip() for x in f.read().split("\n")]
+
+    errors=debug(l)
+    for i in errors:print(i)
+    print(l)
+read_assembly("Ex_test_4.txt")
+
+
+
+    
